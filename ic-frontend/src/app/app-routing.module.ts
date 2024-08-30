9bbd17d53280eb53fb9b08876f260ff821ff78e5
Eduardo Coelho <coelho.edu96@gmail.com>
2024-08-29 21:19:37 -0300

    Added initial auth routes structure and login component.

---
+++ ic-frontend/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  // Redirects to login by default
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+];
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
