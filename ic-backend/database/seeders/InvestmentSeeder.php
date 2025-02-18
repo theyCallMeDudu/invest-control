c8c7b077a04c5bae24ebf8dd4f90de73d6cc1f1d
Eduardo Coelho <coelho.edu96@gmail.com>
2025-02-18 19:42:49 -0300

    IC-0007: Populated investment seeder.

--- ic-backend/database/seeders/InvestmentSeeder.php
+++ ic-backend/database/seeders/InvestmentSeeder.php
@@ -20,15 +20,95 @@ class InvestmentSeeder extends Seeder
             ],
             [
                 'investment_type_id' => 1,
+                'investment_name' => 'BTLG11'
+            ],
+            [
+                'investment_type_id' => 1,
                 'investment_name' => 'EVBI11'
+            ],
+            [
+                'investment_type_id' => 1,
+                'investment_name' => 'HGCR11'
+            ],
+            [
+                'investment_type_id' => 1,
+                'investment_name' => 'HGLG11'
+            ],
+            [
+                'investment_type_id' => 1,
+                'investment_name' => 'HGRE11'
+            ],
+            [
+                'investment_type_id' => 1,
+                'investment_name' => 'HSML11'
+            ],
+            [
+                'investment_type_id' => 1,
+                'investment_name' => 'KNCR11'
+            ],
+            [
+                'investment_type_id' => 1,
+                'investment_name' => 'KNRI11'
+            ],
+            [
+                'investment_type_id' => 1,
+                'investment_name' => 'LVBI11'
+            ],
+            [
+                'investment_type_id' => 1,
+                'investment_name' => 'TVRI11'
+            ],
+            [
+                'investment_type_id' => 1,
+                'investment_name' => 'XPCI11'
             ],
             [
                 'investment_type_id' => 1,
                 'investment_name' => 'XPML11'
             ],
             [
+                'investment_type_id' => 1,
+                'investment_name' => 'VINO11'
+            ],
+            [
+                'investment_type_id' => 2,
+                'investment_name' => 'ABEV3'
+            ],
+            [
+                'investment_type_id' => 2,
+                'investment_name' => 'CAML3'
+            ],
+            [
+                'investment_type_id' => 2,
+                'investment_name' => 'CXSE3'
+            ],
+            [
                 'investment_type_id' => 2,
                 'investment_name' => 'EGIE4F'
+            ],
+            [
+                'investment_type_id' => 2,
+                'investment_name' => 'ITSA4'
+            ],
+            [
+                'investment_type_id' => 2,
+                'investment_name' => 'KLBN11'
+            ],
+            [
+                'investment_type_id' => 2,
+                'investment_name' => 'RADL3'
+            ],
+            [
+                'investment_type_id' => 2,
+                'investment_name' => 'SAPR3'
+            ],
+            [
+                'investment_type_id' => 2,
+                'investment_name' => 'TAEE11'
+            ],
+            [
+                'investment_type_id' => 2,
+                'investment_name' => 'UGPA3'
             ],
         ]);
     }
